414ed490a90f2e4ea1a68d0dafbf8020a7254caa
Pame Plúas <pamepluas@gmail.com>
2020-02-06 00:36:18 -0500

    Adds Order State Tests

---
+++ src/test/java/OrderStateTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class OrderStateTest {
+    private static final String LAKE_FOREST = "Lake Forest";
+    private static final String HIGHLAND_PARK = "Highland Park";
+
+    @Before
+    public void setUp() {
+
+    }
+
+
+}
