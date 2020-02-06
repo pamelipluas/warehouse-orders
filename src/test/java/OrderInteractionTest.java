bd01a726f38bf16ba82a0e988626cc0f5fc9f8a0
Pame Plúas <pamepluas@gmail.com>
2020-02-06 00:38:56 -0500

    Adds Order Interaction Tests File

---
+++ src/test/java/OrderInteractionTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderInteractionTest {
+    private static String LAKE_FOREST = "Lake Forest";
+    
+}
