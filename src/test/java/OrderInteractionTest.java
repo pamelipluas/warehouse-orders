6437625fedce085b0b1571592537ed13a0f52e52
Pame Plúas <pamepluas@gmail.com>
2020-02-09 18:37:03 -0500

    Adds interaction tests

--- src/test/java/OrderInteractionTest.java
+++ src/test/java/OrderInteractionTest.java
@@ -15,5 +15,53 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class OrderInteractionTest {
     private static String LAKE_FOREST = "Lake Forest";
-    
+
+    @Mock
+    Warehouse warehouseMock;
+    MailServiceImpl mailService = Mockito.mock(MailServiceImpl.class);
+
+    @Test
+    public void shouldFillOrderWhenProductIsInStock() {
+        Order order = new Order(LAKE_FOREST, 50);
+        when(warehouseMock.hasStock(order)).thenReturn(true);
+
+        order.fill(warehouseMock);
+
+        verify(warehouseMock, times(1)).withdraw(order);
+        assertTrue(order.isFilled());
+    }
+
+    @Test
+    public void shouldNotFillOrderWhenProductIsOutOfStock() {
+        Order order = new Order(LAKE_FOREST, 51);
+        order.setMailer(mailService);
+        when(warehouseMock.hasStock(order)).thenReturn(false);
+
+        order.fill(warehouseMock);
+
+        verify(warehouseMock, never()).withdraw(order);
+        assertFalse(order.isFilled());
+    }
+
+    @Test
+    public void shouldSendEmailWhenProductIsOutOfStock() {
+        Order order = new Order(LAKE_FOREST, 51);
+        order.setMailer(mailService);
+        when(warehouseMock.hasStock(order)).thenReturn(false);
+
+        order.fill(warehouseMock);
+
+        verify(mailService, times(1)).send(any(Message.class));
+    }
+
+    @Test
+    public void shouldNotSendEmailWhenProductIsInStock() {
+        Order order = new Order(LAKE_FOREST, 50);
+        order.setMailer(mailService);
+        when(warehouseMock.hasStock(order)).thenReturn(true);
+
+        order.fill(warehouseMock);
+
+        verify(mailService, never()).send(any(Message.class));
+    }
 }
